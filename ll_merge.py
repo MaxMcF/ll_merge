50d1e7c944d1c2cf4687334d0545ba75f1837d6f
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-08-22 15:39:57 -0700

    added docstrings

--- ll_merge.py
+++ ll_merge.py
@@ -1,5 +1,14 @@
 
 def ll_merge(ll_1, ll_2):
+    '''Function that merges two linked lists, using a zipper method. The first linked lists
+    head node will be inserted first, followed by the second linked lists head node, followed
+    by the following node in the first linked list, and so on
+        Args:
+            Linked List 1: This should be a singlely linked list of nodes
+            Linked List 2: This should also be a singlely linked list of nodes
+        Return:
+            The head node of the new, merged list
+    '''
     actual_head = ll_1.head
     while ll_1.head is not None and ll_2.head is not None:
         temp = ll_1.head.next
